9eddee4b09db09d278975583fbd736fd0ed46294
emiliencgm <13121570706@163.com>
2023-08-03 15:17:38 +0800

    8.3.3

--- code/run-any.py
+++ code/run-any.py
@@ -57,18 +57,24 @@ elif args.method == 'GTN':
 elif args.method == 'SGL-ED':
     if args.dataset in ['yelp2018', 'gowalla', 'amazon-book', 'last-fm', 'ifashion']:
         p_drop = 0.1
+        temp_tau = 0.2
+        lambda1 = 0.1
         os.system(f'python main.py --project {project} --name {args.method} --notes _ --tag SGL --group baseline --job_type {args.dataset} --model SGL --loss BPR_Contrast --augment ED --lambda1 {lambda1} --p_drop {p_drop} --temp_tau {temp_tau} \
                     --dataset {args.dataset} --cuda {args.device} --comment _ --if_valid {args.valid} --if_visual {args.visual} --visual_epoch 1 --seed {args.seed} --c {args.c}')
         
 elif args.method == 'SGL-RW':
     if args.dataset in ['yelp2018', 'gowalla', 'amazon-book', 'last-fm', 'ifashion']:
         p_drop = 0.1
+        temp_tau = 0.2
+        lambda1 = 0.1
         os.system(f'python main.py --project {project} --name {args.method} --notes _ --tag SGL --group baseline --job_type {args.dataset} --model SGL --loss BPR_Contrast --augment RW --lambda1 {lambda1} --p_drop {p_drop} --temp_tau {temp_tau} \
                     --dataset {args.dataset} --cuda {args.device} --comment _ --if_valid {args.valid} --if_visual {args.visual} --visual_epoch 1 --seed {args.seed} --c {args.c}')
         
 elif args.method == 'SimGCL':
     if args.dataset in ['yelp2018', 'gowalla', 'amazon-book', 'last-fm', 'ifashion']:
         eps_SimGCL = 0.1
+        temp_tau = 0.2
+        lambda1 = 0.1
         os.system(f'python main.py --project {project} --name {args.method} --notes _ --tag {args.method} --group baseline --job_type {args.dataset} --model SimGCL --loss BPR_Contrast --augment No --lambda1 {lambda1} --eps_SimGCL {eps_SimGCL} --temp_tau {temp_tau} \
                     --dataset {args.dataset} --cuda {args.device} --comment _ --if_valid {args.valid} --if_visual {args.visual} --visual_epoch 1 --seed {args.seed} --c {args.c}')
         
@@ -79,6 +85,8 @@ elif args.method == 'PDA':
                     --dataset {args.dataset} --cuda {args.device} --comment _ --if_valid {args.valid} --if_visual {args.visual} --visual_epoch 4 --seed {args.seed} --c {args.c}')
         
 elif args.method == 'BC':
+    temp_tau = 0.1
+    lambda1 = 0.1
     if args.dataset in ['yelp2018', 'gowalla', 'amazon-book', 'last-fm', 'ifashion']:
         os.system(f'python main.py --project {project} --name {args.method} --notes _ --tag {args.method} --group baseline --job_type {args.dataset} --model LightGCN --loss BC --augment No --lambda1 {lambda1} --temp_tau_pop {temp_tau} --temp_tau {temp_tau} \
                     --dataset {args.dataset} --cuda {args.device} --comment _ --if_valid {args.valid} --if_visual {args.visual} --visual_epoch 5 --seed {args.seed} --c {args.c}')
